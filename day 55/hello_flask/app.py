ab69c1f335612e83b26d038cd678ac66a2a7028c
Swati Bansal <65107802+Swati2212@users.noreply.github.com>
2021-07-08 19:23:54 +0530

    Update app.py

--- day 55/hello_flask/app.py
+++ day 55/hello_flask/app.py
@@ -1,1 +1,18 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def say_hello():
+    response_data = {
+        "Status": 200,
+        "Message": "Up and Running",
+        "Developed By": "Swati Bansal"
+    }
+    return response_data, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
